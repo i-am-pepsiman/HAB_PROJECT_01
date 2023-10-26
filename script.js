f28009d5b643e0f3c82369eb76b337206327ef97
i-am-pepsiman <mani-designs@hotmail.com>
2023-10-27 01:43:20 +0200

    I am willing to push these

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const myButton = document.querySelector('#my_button');
 
 let printHelloMember = () => {
-    let arrNames = ['Rodrigo!', 'Cristina!', 'Andrea!', 'Martin!'];
+    let arrNames = ['RODRIGO!', 'CRISTINA!', 'ANDREA!', 'MARTIN!'];
     let random4 = Math.floor(Math.random() * 4);
     
     console.log(`Hola, bienvenid@ ${arrNames[random4]}`);
