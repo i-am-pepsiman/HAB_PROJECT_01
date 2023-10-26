bc8c8d04ce3d0ea5b66f69eb8a86825fe94d04cf
TesseractedBallsax <112576672+i-am-pepsiman@users.noreply.github.com>
2023-10-27 01:09:52 +0200

    Added some files
    Added files needed for working

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict;"
+
+const myButton = document.querySelector('#my_button');
+
+let printHelloMember = () => {
+    let arrNames = ['Rodrigo', 'Cristina', 'Andrea', 'Martin'];
+    let random4 = Math.floor(Math.random() * 4);
+    
+    console.log(`Hola, bienvenid@ ${arrNames[random4]}`);
+}
+
+myButton.addEventListener('click', printHelloMember);
