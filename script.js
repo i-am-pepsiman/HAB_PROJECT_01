cb84441606103ac19300dfdc59932d8264953cfb
i-am-pepsiman <mani-designs@hotmail.com>
2023-10-28 12:52:51 +0200

    Added JS functions to play soundinstances overlapped & code refactor

--- script.js
+++ script.js
@@ -20,39 +20,15 @@ const sound_snare = document.getElementById('sound_snare');
 const sound_tom_high = document.getElementById('sound_tom_high');
 const sound_tom_low = document.getElementById('sound_tom_low');
 const sound_tom_mid = document.getElementById('sound_tom_mid');
-
-// Play sounds on key presses specifically:
-const keySounds = {
-    q: sound_hihat_close,
-    w: sound_hihat_open,
-    e: sound_ride,
-    r: sound_crash,
-    a: sound_tom_low,
-    s: sound_tom_mid,
-    d: sound_snare,
-    f: sound_tom_high,
-    c: sound_kick
-}
-
-function playAudioOnKeypress(key) {
-    if (keySounds[key]) {
-        const audioElement = keySounds[key];
-        audioElement.play();
-    }
-}
-
-document.addEventListener("keypress", (event) => {
-    const keyPressed = event.key;
-    playAudioOnKeypress(keyPressed);
-  });
   
-
 ///////////////////////////////////////////////
 
 // Each function allows to play sound in overlapping instances, so the .wav doesn't need to finish playing before the next one starts
+
 function playAudioCrash() {
     const newAudioCrash = sound_crash.cloneNode(true);
     newAudioCrash.play();
+    
 }
 
 function playAudioHiHatClose() {
@@ -95,116 +71,87 @@ function playAudioTomMid() {
     newAudioTomMid.play();
 }
 
-
 ///////////////////////////////////////////////////////////////////
-
 
 // Event handling for each element
 
-// Crash on click / keypress
+// Crash on click
 button_crash.addEventListener('click', () => {
     playAudioCrash();
 });
-/* button_crash.addEventListener('keypress', (event) => {
-    event.preventDefault();
-    let keyPressed = event.key;
-    if (keyPressed == 'r') {
-        playAudioCrash();
-    }
-}); */
 
-// HiHat Close on click / keypress
+// HiHat Close on click
 button_hihat_close.addEventListener('click', () => {
     playAudioHiHatClose();
 });
-button_hihat_close.addEventListener('keypress', (event) => {
-    event.preventDefault();
-    let keyPressed = event.key;
-    if (keyPressed == 'q') {
-        playAudioHiHatClose();
-    }
-});
 
-// HiHat Open on click / keypress
+// HiHat Open on click
 button_hihat_open.addEventListener('click', () => {
     playAudioHiHatOpen();
 });
-button_hihat_open.addEventListener('keypress', (event) => {
-    event.preventDefault();
-    let keyPressed = event.key;
-    if (keyPressed == 'w') {
-        playAudioHiHatOpen();
-    }
-});
 
-// Kick on click / keypress
+// Kick on click
 button_kick.addEventListener('click', () => {
     playAudioKick();
 });
-button_kick.addEventListener('keypress', (event) => {
-    event.preventDefault();
-    let keyPressed = event.key;
-    if (keyPressed == 'c') {
-        playAudioKick();
-    }
-});
 
-// Ride on click / keypress
+// Ride on click
 button_ride.addEventListener('click', () => {
     playAudioRide();
 });
-button_ride.addEventListener('keypress', (event) => {
-    event.preventDefault();
-    let keyPressed = event.key;
-    if (keyPressed == 'e') {
-        playAudioRide();
-    }
-});
 
-// Snare on click / keypress
+// Snare on click
 button_snare.addEventListener('click', () => {
     playAudioSnare();
 });
-button_snare.addEventListener('keypress', (event) => {
-    event.preventDefault();
-    let keyPressed = event.key;
-    if (keyPressed == 'd') {
-        playAudioSnare();
-    }
-});
 
-// Tom High on click / keypress
+// Tom High on click
 button_tom_high.addEventListener('click', () => {
     playAudioTomHigh();
 });
-button_tom_high.addEventListener('keypress', (event) => {
-    event.preventDefault();
-    let keyPressed = event.key;
-    if (keyPressed == 'f') {
-        playAudioTomHigh();
-    }
-});
 
-// Tom Low on click / keypress
+// Tom Low on click
 button_tom_low.addEventListener('click', () => {
     playAudioTomLow();
 });
-button_tom_low.addEventListener('keypress', (event) => {
-    event.preventDefault();
-    let keyPressed = event.key;
-    if (keyPressed == 'a') {
-        playAudioTomLow();
-    }
-});
 
-// Tom Mid on click / keypress
+// Tom Mid on click
 button_tom_mid.addEventListener('click', () => {
     playAudioTomMid();
 });
-button_tom_mid.addEventListener('keypress', (event) => {
-    event.preventDefault();
-    let keyPressed = event.key;
-    if (keyPressed == 's') {
-        playAudioTomMid();
+
+document.addEventListener('keydown', (event) => {
+    const keyPressed = event.key;
+
+    switch (keyPressed) {
+        case 'r':
+            playAudioCrash();
+            break;
+        case 'q':
+            playAudioHiHatClose();
+            break;
+        case 'w':
+            playAudioHiHatOpen();
+            break;
+        case 'c':
+            playAudioKick();
+            break;
+        case 'e':
+            playAudioRide();
+            break;
+        case 'd':
+            playAudioSnare();
+            break;
+        case 'f':
+            playAudioTomHigh();
+            break;
+        case 'a':
+            playAudioTomLow();
+            break;
+        case 's':
+            playAudioTomMid();
+            break;
+        default:
+            break;
     }
 });
